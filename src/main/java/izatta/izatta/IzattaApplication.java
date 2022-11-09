aa7b0c331953dddbebfa0aae444881005d0eb9cb
Jonata <manojhonson181@gmail.com>
2022-11-09 20:54:12 -0300

    first commit

---
+++ src/main/java/izatta/izatta/IzattaApplication.java
@@ -0,0 +1,13 @@
+package izatta.izatta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzattaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzattaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/izatta/izatta/IzattaApplicationTests.java
@@ -0,0 +1,13 @@
+package izatta.izatta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzattaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
